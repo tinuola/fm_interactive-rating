cb13a39695e7ec915e8d54b7d938b7a0e9b2a7c4
tinuola <24995224+tinuola@users.noreply.github.com>
2023-02-20 01:44:03 -0800

    Checks state of other rating items

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-console.log('hello')
-
 const ratingItems = document.querySelectorAll('.rating-item')
-// console.log(ratingItems)
 
 const ratingOpen = document.querySelector('.wrapper-rating')
 
 const ratingClose = document.querySelector('.wrapper-thankyou')
 
+const displayRating = document.querySelector('.rating-msg')
+
 const submitBtn = document.querySelector('.submit-btn')
-// console.log(submitBtn)
 
 let ratingValue = ''
-// console.log(ratingValue)
 
-ratingItems.forEach( rating => {
+
+ratingItems.forEach( (rating,idx) => {
   rating.addEventListener('click', function(){
-    // console.log('rating')
-    // console.log(rating.innerText)
     ratingValue = rating.innerText
-
-    // rating.classList.toggle('rating-item--focus')
-  //   rating.classList.toggle('rating-item--selected')
+    console.log(ratingValue)
+    // check if it has selected state
+    // Add selected state
+    // Remove focus state
+    // Check and remove selected state from any other state
+    rating.classList.toggle('rating-item--focus')
+    rating.classList.toggle('rating-item--selected')
+    checkOtherRatings(idx)
   })
 })
 
+const checkOtherRatings = index => {
+  ratingItems.forEach( (rating, i) => {
+    if(i !== index){
+      rating.classList.remove('rating-item--selected')
+      rating.classList.add('rating-item--focus')
+    }
+  })
+}
+
 submitBtn.addEventListener('click', function(){
-  console.log(ratingValue)
+  // Display rating message
   ratingOpen.style.display = 'none'
   ratingClose.style.display = 'block'
 })
