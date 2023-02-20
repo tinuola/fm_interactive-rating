08cbb6196bdc9b38a5bfca669a0b8779a920e04e
tinuola <24995224+tinuola@users.noreply.github.com>
2023-02-20 01:55:07 -0800

    Completes JS implementation

--- js/script.js
+++ js/script.js
@@ -14,28 +14,25 @@ let ratingValue = ''
 ratingItems.forEach( (rating,idx) => {
   rating.addEventListener('click', function(){
     ratingValue = rating.innerText
-    console.log(ratingValue)
-    // check if it has selected state
-    // Add selected state
-    // Remove focus state
-    // Check and remove selected state from any other state
     rating.classList.toggle('rating-item--focus')
     rating.classList.toggle('rating-item--selected')
-    checkOtherRatings(idx)
+    deselectOtherRatings(idx)
   })
 })
 
-const checkOtherRatings = index => {
-  ratingItems.forEach( (rating, i) => {
-    if(i !== index){
+
+const deselectOtherRatings = index => {
+  ratingItems.forEach( (rating, idx) => {
+    if(idx !== index){
       rating.classList.remove('rating-item--selected')
       rating.classList.add('rating-item--focus')
     }
   })
 }
 
+
 submitBtn.addEventListener('click', function(){
-  // Display rating message
+  displayRating.textContent = `You selected ${ratingValue} out of 5`
   ratingOpen.style.display = 'none'
   ratingClose.style.display = 'block'
 })
