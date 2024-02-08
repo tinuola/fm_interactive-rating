e08654664045f05900c2bc2b0e0215ba4f98db00
tinuola <24995224+tinuola@users.noreply.github.com>
2024-02-07 21:17:36 -0800

    feat: add error message

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ const ratingCardClose = document.querySelector('.wrapper-thankyou')
 
 const ratingMsg = document.querySelector('.rating-msg')
 
+const errorMsg = document.getElementById('error-msg')
+
 const submitBtn = document.querySelector('.submit-btn')
 
 let ratingValue = ''
-
+let errorState = false
 
 
 submitBtn.addEventListener('click', function(e){
@@ -19,12 +21,35 @@ submitBtn.addEventListener('click', function(e){
     if(rating.checked) {
       ratingValue = rating.getAttribute('data-number')
       displayResult(ratingValue)
+    } else {
+      displayError()
     }
   })
 })
+
+
+// Clear error message when a rating is selected
+ratings.forEach( rating => {
+    rating.addEventListener('click', clearError)
+})
+
 
 function displayResult(num){
   ratingMsg.textContent = `You selected ${num} out of 5`
   ratingCardOpen.style.display = 'none'
   ratingCardClose.style.display = 'block'
+}
+
+
+function displayError(){
+  errorState = true
+  errorMsg.style.display = 'block'
+}
+
+
+function clearError(){
+  if(errorState){
+    errorMsg.style.display = 'none'
+    errorState = false
+  }
 }
