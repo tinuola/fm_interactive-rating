0b0d3d618d87b489bd031cb3a37efbcd669f1c78
tinuola <24995224+tinuola@users.noreply.github.com>
2024-02-07 20:42:04 -0800

    refactor: update markup for accessibility

--- js/script.js
+++ js/script.js
@@ -11,45 +11,20 @@ const submitBtn = document.querySelector('.submit-btn')
 let ratingValue = ''
 
 
-ratings.forEach( (rating,idx) => {
-  rating.addEventListener('click', function(){
-    ratingValue = rating.innerText
-    rating.classList.toggle('rating-item--initial-state')
-    rating.classList.toggle('rating-item--selected-state')
-    deselectOtherRatings(idx)
+
+submitBtn.addEventListener('click', function(e){
+  e.preventDefault()
+
+  ratings.forEach( (rating) => {
+    if(rating.checked) {
+      ratingValue = rating.getAttribute('data-number')
+      displayResult(ratingValue)
+    }
   })
 })
 
-
-// Ensure only one rating has "selected-state"
-const deselectOtherRatings = index => {
-  ratings.forEach( (rating, idx) => {
-    if(idx !== index){
-      rating.classList.remove('rating-item--selected-state')
-      rating.classList.add('rating-item--initial-state')
-    }
-  })
+function displayResult(num){
+  ratingMsg.textContent = `You selected ${num} out of 5`
+  ratingCardOpen.style.display = 'none'
+  ratingCardClose.style.display = 'block'
 }
-
-
-// Confirm that a rating has been selected
-const checkSelectedState = () => {
-  for(let rating of ratings){
-    const isSelected = rating.classList.contains('rating-item--selected-state')
-    if(isSelected){
-      return true;
-    }
-  }
-}
-
-
-submitBtn.addEventListener('click', function(){
-  let hasRating = checkSelectedState()
-
-  if(ratingValue && hasRating){
-    ratingMsg.textContent = `You selected ${ratingValue} out of 5`
-    ratingCardOpen.style.display = 'none'
-    ratingCardClose.style.display = 'block'
-  }
-})
-
